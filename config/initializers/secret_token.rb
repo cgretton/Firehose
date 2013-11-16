c8b9360403b917b88af616952b8cbea9b8b0de16
cgretton <c.gretton@griffith.edu.au>
2013-11-16 18:45:05 +1100

    new changes

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Firehose::Application.config.secret_key_base = '266291c70b0f23f2e4bb32d95384f0a03192eb9f9422e4840f7f1800c339e3e6f9295b70d94b2372b7be07713d644c38800eadad2df67c398fbf88a0b18fff2c'
+Firehose::Application.config.secret_key_base = '7cc368cf787f1f5f7723111a1908a5a77185c789c684df2d56651a6af192c7ce93119eaef0787c15ab07da8c9791de22bfb606a5f989863bf76dde8438b129d2'
